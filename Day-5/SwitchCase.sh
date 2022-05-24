0dcabecbf736608628b1b3bdb39178729091c0c6
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2022-05-24 10:47:58 -0700

    first assignment

---
+++ Day-5/SwitchCase.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+Fruit=apple
+
+case $Fruit in
+               "apple" )
+                       echo "Apple is juicy"
+                ;;
+               "banana")
+                       echo "banana is yellow"
+                ;;
+               "kiwi")
+                       echo "Kiwi is sour"
+                ;;
+esac
